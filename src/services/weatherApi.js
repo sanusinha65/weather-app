ef8a6f201d1e7921b2dc48ae64bad2dc5fbb1826
Aditya Dhanraj <sanusinha65@gmail.com>
2026-06-08 12:17:55 +0530

    Commented out Base URL

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.openweathermap.org/data/2.5";
+// const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const getCurrentWeather = async (lat, lon, units = "metric") => {
